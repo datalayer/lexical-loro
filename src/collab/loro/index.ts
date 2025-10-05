a3c6d321b5757d3be8be15ea08ef48a50f128565
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2025-10-05 17:19:56 -0500

    Export all node types from loro collab module for compatibility

--- src/collab/loro/index.ts
+++ src/collab/loro/index.ts
@@ -4,3 +4,27 @@ export * from './LexicalCollaborationPlugin';
 export * from './State';
 export * from './useCollaboration';
 export * from './wsProvider';
+
+// Export all node types for collaboration compatibility
+export { AutocompleteNode } from '../../nodes/AutocompleteNode';
+export { CounterNode } from '../../nodes/CounterNode';
+export { DateTimeNode } from '../../nodes/DateTimeNode/DateTimeNode';
+export { EmojiNode } from '../../nodes/EmojiNode';
+export { EquationNode } from '../../nodes/EquationNode';
+export { ExcalidrawNode } from '../../nodes/ExcalidrawNode';
+export { FigmaNode } from '../../nodes/FigmaNode';
+export { ImageNode } from '../../nodes/ImageNode';
+export { InlineImageNode } from '../../nodes/InlineImageNode/InlineImageNode';
+export { KeywordNode } from '../../nodes/KeywordNode';
+export { LayoutContainerNode } from '../../nodes/LayoutContainerNode';
+export { LayoutItemNode } from '../../nodes/LayoutItemNode';
+export { MentionNode } from '../../nodes/MentionNode';
+export { PageBreakNode } from '../../nodes/PageBreakNode';
+export { PollNode } from '../../nodes/PollNode';
+export { SpecialTextNode } from '../../nodes/SpecialTextNode';
+export { StickyNode } from '../../nodes/StickyNode';
+export { TweetNode } from '../../nodes/TweetNode';
+export { YouTubeNode } from '../../nodes/YouTubeNode';
+export { CollapsibleContainerNode } from '../../plugins/CollapsiblePlugin/CollapsibleContainerNode';
+export { CollapsibleContentNode } from '../../plugins/CollapsiblePlugin/CollapsibleContentNode';
+export { CollapsibleTitleNode } from '../../plugins/CollapsiblePlugin/CollapsibleTitleNode';
